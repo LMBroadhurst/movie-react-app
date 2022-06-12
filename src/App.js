f66b2c7dac8e7ec8e308505b7615ca67f875880f
Lewis Broadhurst <lewis1broadhurst@gmail.com>
2022-06-12 12:30:22 +0100

    movie container complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,67 @@
+import { useEffect } from "react";
+import './App.css'
+import SearchIcon from './search.svg';
+
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=405bf4ad';
 
+const movie1 = {
+    "Title": "Superman III",
+    "Year": "1983",
+    "imdbID": "tt0086393",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMzI3ZDllMTctNmI2Mi00OGQ4LTk2ZTQtYTJhMjA5ZGI2YmRkXkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_SX300.jpg"
+};
+
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect( () => {
+        searchMovies('Superman');
+    }, []);
+
   return (
-    <div>
-        <h1>App</h1>
+    <div className="app">
+        <h1>MovieLand</h1>
+
+        <div className="search">
+
+            <input 
+            placeholder="Search for movies"
+            value="Superman"
+            onChange={() => {}}
+            />
+
+            <img 
+            src={SearchIcon}
+            alt="search"
+            onClick={() => {}}
+            />
+        </div>
+
+        <div className="container">
+            <div className="movie">
+                <div>
+                    <p>{movie1.Year}</p>
+                </div>
+
+                <div>
+                    <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+                </div>
+
+                <div>
+                    <span>{movie1.Type}</span>
+                    <h3>{movie1.Title}</h3>
+                </div>
+            </div>
+
+        </div>
+        
     </div>
   );
 }
